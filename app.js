5022ccfe2feab2cfce9ad30a6e4f4d626006751f
luiz vasconcellos <luizamboni2002@hotmail.com>
2017-03-17 16:49:34 -0300

    ADD cors middleware

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ let app = require("express")()
 
 global.ENV = process.env.NODE_ENV || "development"
 
+/* helpers */
 let parseQs = require("./helpers/parse-qs")
+
+/* services */
 let ProductPersistence = require("./services/product-persist-service")
+
+/* midleware*/
+let cors = require("cors")
+
+app.use(cors())
 
 app.get("/action/view.png", (req, res) => {
   let { user_id, id, price } = parseQs(req.query)
@@ -33,7 +41,6 @@ app.get("/recommend/who-view-view-too", (req, res) => {
 app.get("/recommend/who-view-buy", (req, res) => {
   let { id } = parseQs(req.query)
 
-  debugger
   ProductPersistence
   .whoClickInBuy(id)
   .then( products => res.json(products) )
