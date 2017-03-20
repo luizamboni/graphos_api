5285aacb0e14188efad30edc83875f158f2a76dd
luiz vasconcellos <luizamboni2002@hotmail.com>
2017-03-20 18:52:04 -0300

    ADD data query

--- scripts/load-data.js
+++ scripts/load-data.js
@@ -11,22 +11,50 @@ let es = require('event-stream')
 
 let db = mongojs(process.env.AFP_MONGO_URL)
 let countClicks = 0
+let countImpressions = 0
 
 const extractClick = string => {
-  let json = string
-            .replace(/^[\[|\[]/,"")
-            .replace(/[\[|\[]$/,"")
-            .replace(/\n/,"")
-            .replace(/^,/,"")
+  try{
+    let json = string
+              .replace(/^[\[|\[]/,"")
+              .replace(/[\[|\[]$/,"")
+              .replace(/\n/,"")
+              .replace(/^,/,"")
 
-  let obj = JSON.parse(json)
+    let obj = JSON.parse(json)
 
-  ProductPersistService
-  .addClickProduct(obj.CP_ID, 
-     parseQs({price: 100.0, id: obj.productInfo[0].pit })
-  )
-  console.log(++countClicks)
+    ProductPersistService
+    .addClickProduct(obj.CP_ID, 
+      parseQs({price: 100.0, id: obj.productInfo[0].pit })
+    )
+    console.log(++countClicks)
+  } catch (err){
+    console.log(err)
+  }
 }
+
+const extractImpressions = string => {
+  try{
+    let json = string
+              .replace(/^[\[|\[]/,"")
+              .replace(/[\[|\[]$/,"")
+              .replace(/\n/,"")
+              .replace(/^,/,"")
+
+    let obj = JSON.parse(json)
+    for(let product of obj.productInfo){
+      ProductPersistService
+      .addViewProduct(obj.CP_ID, 
+        parseQs({price: 100.0, id: product.pit })
+      )
+    }
+
+    console.log(++countImpressions)
+  } catch (err){
+    console.log(err)
+  }
+}
+
 
 const cleanDB = () => ProductPersistService.session
                       .run(`MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r`)
@@ -39,6 +67,13 @@ co(function*(){
   .pipe(JSONStream.stringify())
   .pipe(es.mapSync((data) => {
     extractClick(data)
+    return data
+  }))
+
+  db.impression.find({})
+  .pipe(JSONStream.stringify())
+  .pipe(es.mapSync((data) => {
+    extractImpressions(data)
     return data
   }))
 
